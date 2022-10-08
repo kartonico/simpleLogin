3714a037bf23bd0178cb36ae089570228158b534
Kartonico <luizmiguelsilva488@gmail.com>
2022-10-08 12:25:33 -0300

    Updating system login.

--- Login.py
+++ Login.py
@@ -1,15 +1,19 @@
-import pysimplegui as sg
+import PySimpleGUI as sg
 
 #Creating a layout:
 layout = [
     [sg.Text('User:'), sg.Input('', key='name')],
-    [sg.Text('Password:'), sg.input('', key='Pass')],
-    [sg.Button('Log in', key='logIn'), sg.Button('Register', key='new')],
+    [sg.Text('Password:'), sg.Input('', key='pass')],
+    [sg.Button('Login', key='logIn'), sg.Button('Register', key='new')],
     [sg.Text('', key='msg')],
 ]
 
 #Creating a window:
 window = sg.Window('Login', layout)
+
+#test user:
+Name = 'luiz'
+Pass = '123'
 
 #Opening the window:
 while True:
@@ -17,13 +21,12 @@ while True:
     if event == sg.WIN_CLOSED: #To close the window.
         break
 
-    if values('name') == Name and event == logIn:
+    if values['name'] == Name and values['pass'] == Pass and event == 'logIn':
+        sg.Popup('Teste feito com sucesso.')
         break
-        elife values('name') != Name and event == logIn:
-            sg.Popup('Your username or password is wrong.')
-            
-    #test user:
-    Name = 'luiz'
-    Pass = '123'
+
+    if values['name'] != Name or values['pass'] != Pass and event == 'logIn': #Name wrong
+        sg.Popup('Your username or password is wrong.')
+    
 
 window.close()
